22ab10bfaca32fb3be77a53e552cde1a2e60c102
“nyaimi” <haruka.fullmoon.0707@gmail.com>
2022-08-01 21:30:12 +0900

    オブジェクト

--- gatha.js
+++ gatha.js
@@ -2,21 +2,47 @@ const btn = document.getElementById("btn")
 
 const gatha = document.getElementById('typed');
 
-const Names = ["素材/ルフィ.png","素材/チョッパー.png","素材/バギー.png"];
+function getDataFromDB() {
+const info = {
+  weights: "[5, 0.01], [4, 0.1], [3, 0.89]"
+};
+};
+const Names = [
+  {name:"素材/ルフィ.png" , rarity:5, id: 1001},
+  {name:"素材/チョッパー.png",rarity:3 ,id:3001 },
+  {name:"素材/バギー.png",rarity:3 ,id:3002},
+  {name:"素材/ゾロ.png",rarity:4 ,id:4001},
+  {name:"素材/ティーチ.png",rarity:4 ,id:4002},
+  {name:"素材/ドフラミンゴ.png",rarity:5 ,id:1002},
+  {name:"素材/ナミ.png",rarity:3 ,id:3003},
+  {name:"素材/フランキー.png",rarity:3 ,id:3004},
+  {name:"素材/ブルック.png",rarity:3 ,id:3005},
+  {name:"素材/ママとカイドウ.png",rarity:5 ,id:1003},
+  {name:"素材/もりあ.png",rarity:4 ,id:4003},
+  {name:"素材/ルッチ.png",rarity:3 ,id:3006},
+  {name:"素材/ロビン.png",rarity:3 ,id:3007},
+  {name:"素材/旧三大将.png",rarity:4 ,id:4004}
+];
+
 
 btn.addEventListener("click",function()
 {
-let array = []
+let Names = []
+
 for (let i=0; i<1; i++) {
-  array.push(Math.floor(Math.random() * 3) );
+ 
+  Names.push(Math.floor(Math.random() * 14) );
+
+  if (num === )
 }
-gatha.textContent = array;
+
+gatha.textContent = Names;
 
 const one = document.getElementById("pic");
-one.src = Names[array];
+one.src = Names[Array];
 
 // console.log(Names[array]);
-console.log(array);
+console.log();
 });
 
 
